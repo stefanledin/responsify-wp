1b9919688d347590f590640deacb01770d775463
Stefan Ledin <stefan.ledin@gmail.com>
2014-03-22 20:23:47 +0000

    It is born!

---
+++ picturefill.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Picturefill
+Version: 0.1-alpha
+Description: A helper class for creating the markup required for PictureFill (and more!)
+Author: Stefan Ledin
+Author URI: http://stefanledin.com
+Plugin URI: http://github.com/stefanledin/picturefill
+*/
+
