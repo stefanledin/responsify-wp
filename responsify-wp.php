6e14501eae50b4cb2ac0232bf83b75f94db83df1
stefanledin <stefanledin@gmail.com>
2014-07-15 16:51:10 +0200

    updated version number

--- responsify-wp.php
+++ responsify-wp.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Responsify WP
-Version: 1.3
+Version: 1.4
 Description: A WordPress plugin that creates the markup for responsive images.
 Author: Stefan Ledin
 Author URI: http://stefanledin.com
@@ -17,7 +17,7 @@ require 'includes/content_filter.php';
 
 class Responsify_WP
 {
-	const VERSION = '1.3';
+	const VERSION = '1.4';
 
 	protected static $instance = null;
 
