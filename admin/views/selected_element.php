944822cea28f2323b488a48f2fba14919a3806dd
stefanledin <stefanledin@gmail.com>
2014-08-18 16:46:19 +0200

    first draft of real copy

--- admin/views/selected_element.php
+++ admin/views/selected_element.php
@@ -1,8 +1,12 @@
 <?php 
 $selected_element = get_option( 'selected_element', 'span' );
 ?>
-<h3><?php _e('Select element'); ?></h3>
-<p><?php _e('Should RWP use span och picture?'); ?></p>
+<h3><?php _e('Markup pattern'); ?></h3>
+<p>
+	<?php _e('By default, RWP will use <a href="https://github.com/scottjehl/picturefill/blob/1.2.1/README.md">Picturefill 1.2.1</a> and the markup pattern that uses <b>span</b> elements.'); ?>
+	<br>
+	<?php _e('If you rather want to use the new <b>picture</b> element (<a href="http://scottjehl.github.io/picturefill/">Picturefill 2.1.0</a>), simply select it below.') ?>
+</p>
 <label>
 	<?php _e('span'); ?>
 	<input name="selected_element" type="radio" value="span" <?php echo ( $selected_element == 'span' ) ? 'checked="checked"' : ''; ?>>
@@ -11,4 +15,3 @@ $selected_element = get_option( 'selected_element', 'span' );
 	<?php _e('picture'); ?>
 	<input name="selected_element" type="radio" value="picture" <?php echo ( $selected_element == 'picture' ) ? 'checked="checked"' : ''; ?>>
 </label>
-<?php submit_button( 'Save' ); ?>
