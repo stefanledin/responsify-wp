9670fad1a61f329709525c38aa616633cf3360c4
Stefan Ledin <stefan.ledin@gmail.com>
2014-11-10 21:49:41 +0100

    Forgot to add the settings page to the repo.

---
+++ admin/views/ignored_image_formats.php
@@ -0,0 +1,16 @@
+<h3><?php _e('Ignored image formats'); ?></h3>
+<p><?php _e('RWP should ignore images of the follwing file formats:');?></p>
+<?php
+$file_formats = array( 'jpg', 'png', 'gif' );
+$ignored_image_formats = get_option( 'ignored_image_formats' );
+$html = '<ul>';
+	foreach ( $file_formats as $format ) {
+		$checked_attribute = ($ignored_image_formats[$format]) ? 'checked="checked"' : '';
+		$html .= '<li>';
+			$html .= '<label>';
+				$html .= '<input '.$checked_attribute.' name="ignored_image_formats['.$format.']" type="checkbox">'.$format;
+			$html .= '</label>';
+		$html .= '</li>';
+	}
+$html .= '</ul>';
+echo $html;
