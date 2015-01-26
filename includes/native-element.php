8db493df727c8973d37eb87c044e592f17ceb40e
Stefan Ledin <stefan.ledin@gmail.com>
2015-01-26 05:52:18 +0100

    bug fix, media attribute

--- includes/native-element.php
+++ includes/native-element.php
@@ -13,7 +13,7 @@ class Native_Element extends element
 		$markup = '<picture '.$picture_attributes.'>';
 			for ($i=0; $i < count($this->images)-1; $i++) { 
 				$media_attribute = $this->media_attribute( $this->images[$i] );
-				$markup .= '<source '.$source_attributes.' srcset="'.$this->images[$i]['src'].'" '.$media_attribute.'>';
+				$markup .= '<source '.$source_attributes.' srcset="'.$this->images[$i]['src'].'" media="'.$media_attribute.'">';
 			}
 			$markup .= '<source '.$source_attributes.' srcset="'.$this->images[count($this->images)-1]['src'].'">';
 			$markup .= '<img src="'.$this->images[0]['src'].'" '.$img_attributes.'>';
