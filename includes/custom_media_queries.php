82c37c4e55dc9f35d974a965d8632277d900a609
Stefan Ledin <stefan.ledin@gmail.com>
2015-07-11 13:19:31 +0200

    Working on extracting each rule to it's own function

--- includes/custom_media_queries.php
+++ includes/custom_media_queries.php
@@ -9,6 +9,12 @@ class Custom_Media_Queries {
 		$this->custom_media_queries = $custom_media_queries;
 	}
 
+	/**
+	 * Checks if the media query should be applied in this context.
+	 * @param  string $type 
+	 * @param  array|object $data 
+	 * @return boolean
+	 */
 	public function should_be_applied_when( $type, $data )
 	{
 		$this->rwp_settings = array();
@@ -17,6 +23,10 @@ class Custom_Media_Queries {
 		return ( count($this->rwp_settings) > 0 );
 	}
 
+	/**
+	 * Returns the latest array of settings that has been generated
+	 * @return array
+	 */
 	public function get_settings()
 	{
 		$rwp_settings = $this->rwp_settings;
@@ -29,11 +39,21 @@ class Custom_Media_Queries {
 		foreach ( $this->custom_media_queries as $media_query ) {
 			if ( $media_query['rule']['default'] == 'true' ) {
 				$this->apply_custom_media_queries( $media_query );
+				return;
 			}
 
 			$key = $media_query['rule']['when']['key'];
 			$value = $media_query['rule']['when']['value'];
-			if ( $media_query['rule']['when']['compare'] == '==' ) {
+			$compare = $media_query['rule']['when']['compare'];
+
+			$rule_to_check = 'check_rule_when_';
+			$rule_to_check .= str_replace('-', '_', $key) . '_';
+			$rule_to_check .= ( $compare == '==' ) ? 'equals' : 'not_equals';
+			if ( call_user_func( array($this, $rule_to_check), $post_object, $value ) ) {
+				$this->apply_custom_media_queries( $media_query );
+			}
+
+			/*if ( $media_query['rule']['when']['compare'] == '==' ) {
 				if ( $key == 'page-id' ) {
 					if ( $post_object->ID == (int) $value ) {
 						$this->apply_custom_media_queries( $media_query );
@@ -55,11 +75,28 @@ class Custom_Media_Queries {
 						$this->apply_custom_media_queries( $media_query );
 					}
 				}
-			}
-			
+			}*/
 			
 		}
 	}
+	protected function check_rule_when_page_id_equals( $post_object, $value )
+	{
+		return ( $post_object->ID == (int) $value );
+	}
+	protected function check_rule_when_page_id_not_equals( $post_object, $value )
+	{
+		return ( $post_object->ID != (int) $value );
+	}
+
+	protected function check_rule_when_page_slug_equals( $post_object, $value )
+	{
+		return ( $post_object->post_name == $value );
+	}
+	protected function check_rule_when_page_slug_not_equals( $post_object, $value )
+	{
+		return ( $post_object->post_name != $value );
+	}
+
 	protected function check_rule_for_image( $attributes )
 	{
 		foreach ($this->custom_media_queries as $media_query) {
