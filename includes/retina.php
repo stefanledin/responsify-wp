0f762948d4815fa59df7afad7bcbf5ba40791960
Stefan Ledin <stefan.ledin@gmail.com>
2015-09-11 14:39:23 +0200

    Bugfix. Retina => true + array with selected image sizes is working now

--- includes/retina.php
+++ includes/retina.php
@@ -3,27 +3,40 @@ class Retina
 {
     protected $settings;
     protected $image_sizes;
+    protected $all_image_sizes;
 
     function __construct( $settings )
     {
         $this->settings = $settings;
+        $this->all_image_sizes = get_intermediate_image_sizes();
     }
 
     public function set_sizes( $sizes )
     {
         $this->image_sizes = $sizes;
         
+        if ( $this->settings['retina'] == true ) {
+            $this->add_all_retina_sizes();
+        }
+        
         if ( !is_bool($this->settings['retina']) ) {
+            
             if ( isset($this->settings['sizes']) ) {
                 $this->add_retina_sizes();
             }
             $this->set_retina_sizes();
-        }
-        if ( !$this->settings['retina'] ) {
+        } 
+        
+        if ( $this->settings['retina'] == false ) {
             $this->remove_retina_sizes();
         }
 
         return $this->image_sizes;
+    }
+
+    protected function is_retina_size( $size )
+    {
+        return strpos($size, '@');
     }
 
     protected function set_retina_sizes()
@@ -34,7 +47,7 @@ class Retina
         $image_sizes = array();
         foreach ( $densities as $density ) {
             foreach ( $this->image_sizes as $image_size ) {
-                if ( (!strpos($image_size, '@')) || (strpos($image_size, $density)) ) {
+                if ( (!$this->is_retina_size($image_size)) || (strpos($image_size, $density)) ) {
                     if ( !in_array($image_size, $image_sizes) ) {
                         array_push($image_sizes, $image_size);
                     }
@@ -44,6 +57,15 @@ class Retina
         $this->image_sizes = $image_sizes;
     }
 
+    protected function add_all_retina_sizes()
+    {
+        foreach ($this->all_image_sizes as $image_size) {
+            if ( $this->is_retina_size( $image_size ) && !in_array($image_size, $this->image_sizes) ) {
+                array_push($this->image_sizes, $image_size);
+            }
+        }
+    }
+
     protected function add_retina_sizes()
     {
         $densities = ( is_array($this->settings['retina']) )
@@ -51,7 +73,7 @@ class Retina
             : array( $this->settings['retina'] );
         foreach ($densities as $density) {
             foreach ( $this->image_sizes as $image_size ) {
-                if ( !strpos($image_size, '@') ) {
+                if ( !$this->is_retina_size($image_size) ) {
                     array_push($this->image_sizes, $image_size.'@'.$density);
                 }
             }
@@ -62,7 +84,7 @@ class Retina
     {
         $count = count($this->image_sizes);
         for ($i=0; $i < $count; $i++) { 
-            if ( strpos($this->image_sizes[$i], '@') ) {
+            if ( $this->is_retina_size($this->image_sizes[$i]) ) {
                 unset($this->image_sizes[$i]);
             }
         }
