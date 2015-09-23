b948e03a88b6d0d329e8edea3d9fabd27e515133
Stefan Ledin <stefan.ledin@gmail.com>
2015-09-23 14:46:50 +0200

    Copy/paste mistake...

--- admin/views/debug_mode.php
+++ admin/views/debug_mode.php
@@ -9,11 +9,11 @@
 			<?php _e('Add HTML comments with handy information about the image and what RWP could or could not do.'); ?>
 		</p>
 		<label>
-			<?php _e('On (recommended)'); ?>
+			<?php _e('On'); ?>
 			<input name="rwp_debug_mode" type="radio" value="on" <?php echo ( $active == 'on' ) ? 'checked="checked"' : ''; ?>>
 		</label>
 		<label>
-			<?php _e('Off (native mode)'); ?>
+			<?php _e('Off'); ?>
 			<input name="rwp_debug_mode" type="radio" value="off" <?php echo (( $active == 'off' ) ? 'checked="checked"' : ''); ?>>
 		</label>
 		<?php submit_button( 'Save' ); ?>
